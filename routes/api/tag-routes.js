2b3a44ba225a1d40de8b0a2e9ec84cee41b677a5
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-22 16:37:47 -0600

    Added put tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,6 +62,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagData) => {
+    if (!tagData) {
+      res.status(404).json({ message: "Tag not found."});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
