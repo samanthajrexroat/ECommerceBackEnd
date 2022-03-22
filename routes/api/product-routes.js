24dfa4ae84868785ce700a762a4339c2d85f1757
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-22 16:15:48 -0600

    Added delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -136,6 +136,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((productData) => {
+    if(!productData) {
+      res.status(404).json({ message: "Product not found."})
+      return;
+    }
+    res.json(productData);
+  })
+  .catch((err)=> {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
