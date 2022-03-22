2a0886d559192fb64619ec069f252fcad855baef
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-22 09:25:19 -0600

    Added find one category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,10 +7,12 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [{
-      model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"]
-    }]
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+      }
+    ]
   })
   .then(categoryData => res.json(categoryData))
   .catch((err) => {
@@ -22,6 +24,28 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+      }
+    ]
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: "No category found"});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
