8e698d495a081908cfff065f280cc27e2f040eb5
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-22 09:38:53 -0600

    Added update category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,6 +62,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((categoryData) => {
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found."});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  }) 
 });
 
 router.delete('/:id', (req, res) => {
